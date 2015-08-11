fa4a4b4baa5a4a7c78f9494d7d027cab0570489f
Miguel Angel Alarcos Torrecillas <miguel.alarcos@gmail.com>
2015-08-11 17:52:13 +0200

    font awesome

--- app/date_time_picker/config/dependencies.rb
+++ app/date_time_picker/config/dependencies.rb
@@ -1,2 +1,4 @@
 # Specify which components you wish to include when
 # this component loads.
+
+component 'font_awesome'
--- app/main/config/dependencies.rb
+++ app/main/config/dependencies.rb
@@ -11,3 +11,5 @@ component 'bootstrap_jumbotron_theme'
 component 'user_templates'
 
 component 'date_time_picker'
+
+
